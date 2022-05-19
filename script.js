d06dd24fa7ed5b6eb2291bd5b0cd90b3894cfd29
mirza-adnan <mirza.adnan2205@gmail.com>
2022-05-20 00:44:40 +0600

    checks for punctuation

--- script.js
+++ script.js
@@ -4,39 +4,46 @@ const output = document.querySelector(".output-text");
 input.addEventListener("input", () => {
     output.textContent = "";
     textSplit = input.value.split(" ");
-
+    console.log(textSplit);
     textSplit.forEach((word) => {
         const boldSpan = document.createElement("span");
         boldSpan.classList.add("bold");
 
         const span = document.createElement("span");
 
-        const len = word.length;
+        if (word) {
+            const len = isPunctuation(word) ? word.length - 1 : word.length;
 
-        if (len === 1 || len == 2) {
-            boldSpan.textContent = word.substring(0, 1);
-            output.appendChild(boldSpan);
+            if (len === 1 || len == 2) {
+                boldSpan.textContent = word.substring(0, 1);
+                output.appendChild(boldSpan);
 
-            span.textContent = word.substring(1) + " ";
-            output.appendChild(span);
-        } else if (len === 3 || len == 4) {
-            boldSpan.textContent = word.substring(0, 2);
-            output.appendChild(boldSpan);
+                span.textContent = word.substring(1) + " ";
+                output.appendChild(span);
+            } else if (len === 3 || len == 4) {
+                boldSpan.textContent = word.substring(0, 2);
+                output.appendChild(boldSpan);
 
-            span.textContent = word.substring(2) + " ";
-            output.appendChild(span);
-        } else if (len === 5 || len == 6) {
-            boldSpan.textContent = word.substring(0, 3);
-            output.appendChild(boldSpan);
+                span.textContent = word.substring(2) + " ";
+                output.appendChild(span);
+            } else if (len === 5 || len == 6) {
+                boldSpan.textContent = word.substring(0, 3);
+                output.appendChild(boldSpan);
 
-            span.textContent = word.substring(3) + " ";
-            output.appendChild(span);
-        } else if (len >= 7) {
-            boldSpan.textContent = word.substring(0, 4);
-            output.appendChild(boldSpan);
+                span.textContent = word.substring(3) + " ";
+                output.appendChild(span);
+            } else if (len >= 7) {
+                boldSpan.textContent = word.substring(0, 4);
+                output.appendChild(boldSpan);
 
-            span.textContent = word.substring(4) + " ";
-            output.appendChild(span);
+                span.textContent = word.substring(4) + " ";
+                output.appendChild(span);
+            }
         }
     });
 });
+
+function isPunctuation(word) {
+    const puncRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    return word[word.length - 1].match(puncRegex);
+}
