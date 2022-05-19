4c19faa4804cb43b2042834d5a154249b9cce568
mirza-adnan <mirza.adnan2205@gmail.com>
2022-05-20 01:16:33 +0600

    fixed bug where new line wouldnt count as a new word

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
 const input = document.querySelector(".input-text");
 const output = document.querySelector(".output-text");
 
-input.addEventListener("input", () => {
+input.addEventListener("input", (e) => {
     output.textContent = "";
-    textSplit = input.value.split(" ");
-    console.log(textSplit);
+
+    textSplit = input.value.split(/(\s)|(?=[\n])|(?<=[\n])/g);
+
     textSplit.forEach((word) => {
         const boldSpan = document.createElement("span");
         boldSpan.classList.add("bold");
 
-        const span = document.createElement("span");
+        const regularSpan = document.createElement("span");
 
         if (word) {
             const len = isPunctuation(word) ? word.length - 1 : word.length;
 
             if (len === 1 || len == 2) {
                 boldSpan.textContent = word.substring(0, 1);
-                output.appendChild(boldSpan);
-
-                span.textContent = word.substring(1) + " ";
-                output.appendChild(span);
+                regularSpan.textContent = word.substring(1);
             } else if (len === 3 || len == 4) {
                 boldSpan.textContent = word.substring(0, 2);
-                output.appendChild(boldSpan);
-
-                span.textContent = word.substring(2) + " ";
-                output.appendChild(span);
+                regularSpan.textContent = word.substring(2);
             } else if (len === 5 || len == 6) {
                 boldSpan.textContent = word.substring(0, 3);
-                output.appendChild(boldSpan);
-
-                span.textContent = word.substring(3) + " ";
-                output.appendChild(span);
+                regularSpan.textContent = word.substring(3);
             } else if (len >= 7) {
                 boldSpan.textContent = word.substring(0, 4);
-                output.appendChild(boldSpan);
-
-                span.textContent = word.substring(4) + " ";
-                output.appendChild(span);
+                regularSpan.textContent = word.substring(4);
             }
+            output.appendChild(boldSpan);
+            output.appendChild(regularSpan);
         }
     });
 });
